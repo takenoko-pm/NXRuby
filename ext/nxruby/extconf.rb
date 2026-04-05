d2eeb304e3877f8fe4b906c02126e112e7f16ab1
takenoko-pm <skazu0611@icloud.com>
2026-04-05 15:32:53 +0900

    linuxビルドの削除

--- ext/nxruby/extconf.rb
+++ ext/nxruby/extconf.rb
@@ -3,10 +3,8 @@ require "mkmf"
 require "fileutils"
 require "mini_portile2"
 
-# ビルドしたレシピを保持しておくハッシュ（依存関係の解決に使う）
 $recipes = {}
 
-# 共有ライブラリ（DLL等）を nxruby.so と同じ階層にコピーする関数
 def copy_shared_libraries(recipe)
   ext = if RUBY_PLATFORM =~ /mingw|mswin/
           "dll"
@@ -16,11 +14,13 @@ def copy_shared_libraries(recipe)
           "so*"
         end
 
-  # bin や lib フォルダにある共有ライブラリをカレントディレクトリにコピー
+  # コピー先を lib/nxruby に指定
+  lib_dir = File.expand_path("../../lib/nxruby", __dir__)
+  FileUtils.mkdir_p(lib_dir)
+
   Dir.glob("#{recipe.path}/**/*.{#{ext}}").each do |lib_file|
-    # .dll.a や .so.a などのインポートライブラリは除外
     next if lib_file.end_with?(".a")
-    FileUtils.cp(lib_file, ".", preserve: true)
+    FileUtils.cp(lib_file, lib_dir, preserve: true)
   end
 end
 
@@ -47,22 +47,17 @@ def build_sdl_component(repo_name, lib_name, version, depends_on = [])
     $LDFLAGS  << " -L#{recipe.path}/lib"
     $libs = append_library($libs, lib_name)
     
-    # LinuxやMacで、実行時に同じフォルダのライブラリを探せるようにする呪文(rpath)
-    if RUBY_PLATFORM =~ /darwin|linux/
-      $LDFLAGS << " -Wl,-rpath,."
+    # Macの場合は実行ファイルと同じ場所を探すようにする
+    if RUBY_PLATFORM =~ /darwin/
+      $LDFLAGS << " -Wl,-rpath,@loader_path/."
     end
     
-    # 完成したDLL等を nxruby.so の横に持ってくる
     copy_shared_libraries(recipe)
-    
     $recipes[repo_name] = recipe
   end
 end
 
-# 1. 本体（依存なし）
 build_sdl_component("SDL", "SDL3", "3.4.4")
-
-# 2. 拡張（SDL本体に依存）
 build_sdl_component("SDL_image", "SDL3_image", "3.4.2", ["SDL"])
 build_sdl_component("SDL_mixer", "SDL3_mixer", "3.2.0", ["SDL"])
 build_sdl_component("SDL_ttf",   "SDL3_ttf",   "3.2.2", ["SDL"])
--- lib/nxruby.rb
+++ lib/nxruby.rb
@@ -1,1 +1,8 @@
+if RUBY_PLATFORM =~ /mingw|mswin/
+  require 'ruby_installer'
+  # 区切り文字を Windows 仕様 (\) に変換してDLLパスを登録
+  dll_dir = File.expand_path("nxruby", __dir__).tr("/", "\\")
+  RubyInstaller::Runtime.add_dll_directory(dll_dir)
+end
+
 require_relative "nxruby/nxruby"
